3f73fc8c2fe4c9559d5a72eec175a58c1a4e955c
winasya <winasya@mail.ru>
2020-06-20 19:31:20 +0300

    based on old tests

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -2,14 +2,17 @@
 #define BIG_INTEGER_H
 
 #include <cstddef>
-#include <gmp.h>
 #include <iosfwd>
+#include <cstdint>
+#include <vector>
+#include <string>
 
 struct big_integer
 {
     big_integer();
     big_integer(big_integer const& other);
     big_integer(int a);
+    big_integer(unsigned a);
     explicit big_integer(std::string const& str);
     ~big_integer();
 
@@ -38,31 +41,50 @@ struct big_integer
     big_integer& operator--();
     big_integer operator--(int);
 
-    friend bool operator==(big_integer const& a, big_integer const& b);
-    friend bool operator!=(big_integer const& a, big_integer const& b);
-    friend bool operator<(big_integer const& a, big_integer const& b);
-    friend bool operator>(big_integer const& a, big_integer const& b);
-    friend bool operator<=(big_integer const& a, big_integer const& b);
-    friend bool operator>=(big_integer const& a, big_integer const& b);
+    friend big_integer operator+(big_integer a, big_integer const& b);
+    friend big_integer operator-(big_integer a, big_integer const& b);
+    friend big_integer operator*(big_integer a, big_integer const& b);
+    friend big_integer operator/(big_integer a, uint32_t const& b);
+    friend big_integer operator/(big_integer a, int const& b);
+    friend big_integer operator/(big_integer a, big_integer const& b);
+    friend big_integer operator%(const big_integer& a, big_integer const& b);
+
+    friend big_integer operator&(big_integer a, big_integer const& b);
+    friend big_integer operator|(big_integer a, big_integer const& b);
+    friend big_integer operator^(big_integer a, big_integer const& b);
+
+    friend big_integer operator<<(big_integer a, unsigned int b);
+    friend big_integer operator>>(big_integer a, unsigned int b);
+
 
     friend std::string to_string(big_integer const& a);
+    friend int8_t compare(big_integer const& a, big_integer const& b);
+
 
 private:
-    mpz_t mpz;
+    big_integer to_bits() const;
+    big_integer from_bits() const;
+    void normalize();
+    std::vector<uint32_t> number;
+    bool sign{};
 };
 
 big_integer operator+(big_integer a, big_integer const& b);
 big_integer operator-(big_integer a, big_integer const& b);
 big_integer operator*(big_integer a, big_integer const& b);
+big_integer operator/(big_integer a, uint32_t const& b);
+big_integer operator/(big_integer a, int const& b);
 big_integer operator/(big_integer a, big_integer const& b);
-big_integer operator%(big_integer a, big_integer const& b);
+big_integer operator%(const big_integer& a, big_integer const& b);
 
 big_integer operator&(big_integer a, big_integer const& b);
 big_integer operator|(big_integer a, big_integer const& b);
 big_integer operator^(big_integer a, big_integer const& b);
 
-big_integer operator<<(big_integer a, int b);
-big_integer operator>>(big_integer a, int b);
+big_integer operator<<(big_integer a, unsigned int b);
+big_integer operator>>(big_integer a, unsigned int b);
+
+int8_t compare(big_integer const& a, big_integer const& b);
 
 bool operator==(big_integer const& a, big_integer const& b);
 bool operator!=(big_integer const& a, big_integer const& b);
